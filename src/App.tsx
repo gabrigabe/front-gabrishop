e7d825c648f37cacb13796c74f65007fac4ad20e
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2023-04-21 14:22:15 -0300

    feat: products Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import Login from './components/login'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './components/register';
-
+import AuthService from './services/AuthService';
+import Products from './components/productsPage'
 
 
 
@@ -15,6 +16,11 @@ function App() {
     },
   });
 
+  function RequireAuth({children, redirectTo}:any){
+    let isAuth = AuthService.checkLogin()
+    return isAuth ? children : <Navigate to={redirectTo}/>
+  }
+
 
   return (
     <div className='App'>
@@ -22,10 +28,15 @@ function App() {
     <CssBaseline/>
 
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/products' element={
+          <RequireAuth redirectTo="/">
+              <Products/>
+          </RequireAuth>}>
+        </Route>
+      </Routes>
     </BrowserRouter>
   
   </ThemeProvider>
---
+++ src/components/productsPage.tsx
@@ -0,0 +1,9 @@
+import NavBarApp from "../assets/navBar";
+
+export default function Products() {
+    return(
+        <>
+        <NavBarApp/>
+        </>
+    )
+}
